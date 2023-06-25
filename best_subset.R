7bd0250fbf7928101f06c5a5bcef4962c3f0d6a1
Juan Martinez <juan.c.martinez@vanderbilt.edu>
2023-06-24 19:57:02 -0500

    Update

--- best_subset.R
+++ best_subset.R
@@ -1,14 +1,11 @@
 library(tidyverse)
 library(sets)
 
-explanatory_vars <- c("PESO_kg","ROTA_CERV_IZQ","ROTA_CERV_DER","FLEX_FRONT_CERV_FLEXION",
-                      "FLEX_FRONT_CERV_EXTENSION","FLEX_LAT_CERV_DERECHA",
-                      "FLEX_LAT_CERV_IZQUIERDA","SCHOBERT_MOD", "FLEX_LUMBAR_LAT_DERECHA",
-                      "FLEX_LUMBAR_LAT_IZQUIERDA", "DISTANCIA_INTERMALEOLAR")
+explanatory_vars <- names(datos)[1:11]
+
 2^(length(explanatory_vars))
 
-
-
+set.seed(123)
 power_set <- unlist(lapply(1:length(explanatory_vars),
                            # Get all combinations
                            combinat::combn,
@@ -28,17 +25,17 @@ datos %>%
 
 confusionMatrix(as_factor(rep('SI', len = nrow(datos))), datos$CONTROLADO)
 
-all_log_reg_models <- list()
 all_log_reg_models_sensitivity <- NA
 all_log_reg_models_specificity <- NA
+len <- NA
 
 steps <- c(1:length(power_set))
 
 for(i in steps){
   
-  len = length(power_set[[i]])
+  len[i] = length(power_set[[i]])
   
-  if(len == 1){
+  if(len[i] == 1){
     formula1 <- as.formula(paste('CONTROLADO', power_set[[i]], sep = ' ~ '))
     print(formula1)
     
@@ -53,7 +50,7 @@ for(i in steps){
     print(message)
     
   }
-  else if(len > 2){
+  else if(len[i] >= 2){
     
     formula1 <- as.formula(paste('CONTROLADO', paste(power_set[[i]], collapse = ' + '), sep = ' ~ '))
     print(formula1)
@@ -75,7 +72,8 @@ hist(all_log_reg_models_specificity)
 
 best_subset_raw_data <- data.frame(
   'Sensitivity' = all_log_reg_models_sensitivity,
-  'Specificity' = all_log_reg_models_specificity
+  'Specificity' = all_log_reg_models_specificity,
+  'Num_Variables' = as_factor(len)
 )
 
 best_specificity <- which.max(all_log_reg_models_specificity)
@@ -83,3 +81,19 @@ best_specificity <- which.max(all_log_reg_models_specificity)
 best_subset_raw_data[best_specificity, ]
 
 power_set[[best_specificity]]
+
+
+best_subset_raw_data %>%
+  filter(is.na(Specificity) == F) %>%
+  group_by(Num_Variables) %>%
+  summarize(Max_Spec = max(Specificity),
+            Mean_Spec = mean(Specificity),
+            SD_Spec = sd(Specificity),
+            Max_Sens = max(Sensitivity)) %>%
+  unique()
+
+
+best_subset_raw_data %>%
+  ggplot(aes(x = Num_Variables, y = Specificity, group = Num_Variables)) +
+  geom_boxplot(aes(fill = Num_Variables)) +
+  theme(legend.position = 'bottom')
